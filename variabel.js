44acbfca43178c95217d45ea4a0e50b8262336f3
unknown <sidikwf@gmail.com>
2023-10-22 15:14:06 +0700

    push pertama

---
+++ variabel.js
@@ -0,0 +1,12 @@
+console.log("start program")
+
+const iniString = "Daksa Darmana";
+
+for (let i=0; i<iniString.length; i++){
+    if (i % 2 == 0) {
+        console.log("haha")
+    }
+    else  {
+        console.log(i)
+    }
+}
